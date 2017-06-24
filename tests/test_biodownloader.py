b880c271e471d53a24008e857e5fb3b019ca520f
Fábio Madeira <fbiomadeira@gmail.com>
2017-06-24 18:58:18 +0100

    Revert "Trying to debug why tests fail on travis..."
    This reverts commit 693bd6eecd7819b516ff6dd2589e21bcedd2dd74.

--- tests/test_biodownloader.py
+++ tests/test_biodownloader.py
@@ -422,13 +422,7 @@ class TestBioDownloader(unittest.TestCase):
             result = runner.invoke(self.downloads, ['pdb', '--pdb', '--override',
                                                     '--output',
                                                     self.tmp, self.pdbid])
-            print()
-            print(result.exc_info)
-            print()
-            print(result.exception)
-            print()
-            print(result.exit_code)
-            # self.assertEqual(result.exit_code, 0)
+            self.assertEqual(result.exit_code, 0)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestBioDownloader)
