7f204d848f73052c0c02c033baf9921ce94efe4a
Fábio Madeira <fbiomadeira@gmail.com>
2017-06-24 17:04:23 +0100

    Added standard installation setup.py which sets up a command line interface 'BioDownloader'.

---
+++ setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    BioDownloader: a Command Line Tool for downloading protein structures,
+    protein sequences and multiple sequence alignments.
+    Copyright (C) 2017  Fábio Madeira
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+from setuptools import setup
+from setuptools import find_packages
+
+__author__ = 'Fábio Madeira'
+__email__ = 'fabiomadeira@me.com'
+__version__ = '0.1.0'
+
+setup(
+    # Basic package information.
+    name='biodownloader',
+    version=__version__,
+    packages=find_packages(exclude=["tests", 'tests*']),
+
+    # Packaging options.
+    include_package_data=True,
+
+    # Package dependencies
+    # should always match the entries in requirements.txt
+    install_requires=[
+        "requests",
+        "responses",
+        "click",
+    ],
+
+    entry_points={
+        "console_scripts": [
+            "BioDownloader=biodownloader.cli:downloads",
+        ]
+    },
+
+    # tests
+    test_suite="tests.test_biodownloader",
+
+    # Metadata for PyPI.
+    author=__author__,
+    author_email=__email__,
+    license='MIT',
+    url='http://github.com/biomadeira/biodownloader/tree/master',
+    keywords='download pdb uniprot sifts cath pfam python cli',
+    description=('BioDownloader: a Command Line Tool for downloading '
+                 'protein structures, protein sequences and '
+                 'multiple sequence alignments.'),
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: PyPy3',
+        'Topic :: Internet',
+        'Topic :: Scientific/Engineering :: Bio-informatics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
+
