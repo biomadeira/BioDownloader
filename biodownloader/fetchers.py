681f7366147803f52dc7a093e1675a9b2af068ca
Fábio Madeira <fbiomadeira@gmail.com>
2017-06-27 19:23:44 +0100

    Added two new classes Fetchers and Downloaders to increase reusability of the codebase.

--- biodownloader/fetchers.py
+++ biodownloader/fetchers.py
@@ -20,14 +20,13 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import gzip
-import logging
 import os
-import pickle
-import requests
-import shutil
 import time
-import urllib
+import gzip
+import pickle
+import shutil
+import logging
+import requests
 
 from biodownloader.config import config
 
@@ -97,6 +96,35 @@ def fetch_from_url_or_retry(url, json=True, header=None, post=False, data=None,
             logger.critical(message)
 
 
+class Fetcher(object):
+    def __init__(self, url, cached=False, cache_output=None, **kwargs):
+        """
+        :param url: (str) Full web-address
+        :param cached: (boolean) if True, stores a pickle file locally
+        :param cache_output: (str) file name if 'cached=True'
+        """
+
+        self.url = url
+        self.cached = cached
+        self.cache_output = cache_output
+        self.pickled = os.path.join(config.db_root, config.db_pickled,
+                                    self.cache_output)
+        self.kwargs = kwargs
+        self.response = None
+        self._fetch()
+
+    def _fetch(self):
+
+        if self.cached and os.path.isfile(self.pickled):
+            self.response = pickle.load(open(self.pickled, 'rb'))
+        else:
+            self.response = fetch_from_url_or_retry(self.url, **self.kwargs)
+            if self.cached:
+                with open(self.pickled, 'wb') as output:
+                    pickle.dump(self.response, output, -1)
+        return self.response
+
+
 def fetch_summary_properties_pdbe(identifier, cached=False, retry_in=(429,)):
     """
     Queries the PDBe API to get summary properties.
@@ -106,17 +134,14 @@ def fetch_summary_properties_pdbe(identifier, cached=False, retry_in=(429,)):
     :param retry_in: http code for retrying connections
     :return: response object
     """
-    pickled = os.path.join(config.db_root, config.db_pickled, identifier+"_sp.pkl")
-    if cached and os.path.isfile(pickled):
-        response = pickle.load(open(pickled, 'rb'))
-    else:
-        pdbe_endpoint = "pdb/entry/summary/"
-        url = config.api_pdbe + pdbe_endpoint + identifier
-        response = fetch_from_url_or_retry(url, retry_in=retry_in, json=True)
-        if cached:
-            with open(pickled, 'wb') as output:
-                pickle.dump(response, output, -1)
-    return response
+
+    url_root = config.api_pdbe
+    url_enpoint = "pdb/entry/summary/"
+    url = url_root + url_enpoint + identifier
+    b = Fetcher(url=url, cached=cached,
+                cache_output="{}_sp.pkl".format(identifier),
+                json=True, retry_in=retry_in)
+    return b.response
 
 
 def get_preferred_assembly_id(identifier):
@@ -154,6 +179,57 @@ def get_preferred_assembly_id(identifier):
     return bio_best
 
 
+class Downloader(object):
+    def __init__(self, url, outputfile, decompress=True, override=False):
+        """
+        :param url: (str) Full web-address
+        :param outputfile: (str) Output filename
+        :param decompress: (boolean) Decompresses the file
+        :param override: (boolean) Overrides any existing file, if available
+        """
+
+        self.url = url
+        self.outputfile = outputfile
+        self.outputfile_origin = outputfile
+        self.decompress = decompress
+        self.override = override
+
+        if self.decompress:
+            if self.outputfile_origin.endswith('.gz'):
+                self.outputfile = self.outputfile_origin.rstrip('.gz')
+
+        if not os.path.exists(self.outputfile) or self.override:
+            self._download()
+        else:
+            logger.info('{} already available...'.format(self.outputfile))
+
+        if self.outputfile_origin.endswith('.gz') and self.decompress:
+            self._decompress()
+
+    def _download(self):
+        try:
+            try:
+                import urllib.request
+                from urllib.error import URLError, HTTPError
+                with urllib.request.urlopen(self.url) as response, \
+                        open(self.outputfile_origin, 'wb') as outfile:
+                    shutil.copyfileobj(response, outfile)
+            except (AttributeError, ImportError):
+                import urllib
+                urllib.urlretrieve(self.url, self.outputfile_origin)
+        except (URLError, HTTPError, IOError, Exception) as e:
+            message = 'Unable to retrieve {} for {}'.format(self.url, str(e))
+            logger.debug(message)
+
+    def _decompress(self):
+        with gzip.open(self.outputfile_origin, 'rb') as infile, \
+                open(self.outputfile, 'wb') as outfile:
+            shutil.copyfileobj(infile, outfile)
+            os.remove(self.outputfile_origin)
+            logger.info('Decompressed {} to {}'
+                        ''.format(self.outputfile_origin, self.outputfile))
+
+
 def download_structure_from_pdbe(identifier, pdb=False, bio=False, override=False):
     """
     Downloads a structure from the PDBe to the filesystem.
@@ -175,36 +251,26 @@ def download_structure_from_pdbe(identifier, pdb=False, bio=False, override=Fals
 
     outputfile = os.path.join(config.db_root, config.db_pdbx, filename)
     os.makedirs(os.path.join(config.db_root, config.db_pdbx), exist_ok=True)
-    if not os.path.exists(outputfile.rstrip('.gz')) or override:
-        if pdb:
-            url = config.http_pdbe + ("entry-files/download/"
-                                      "pdb{}.ent".format(identifier))
-        else:
-            if bio:
-                pref = fetch_preferred_assembly_id(identifier=identifier)
-                endpoint = ("static/entry/download/"
-                            "{}-assembly-{}.cif.gz".format(identifier, pref))
-                # atom lines only?
-                # endpoint = "{}-assembly-{}_atom_site.cif.gz".format(pdbid, pref)
-                url = config.http_pdbe + endpoint
-            else:
-                # original mmCIF?
-                # url = config_defaults.pdbe_download + "{}.cif".format(pdbid)
-                url = config.http_pdbe + ("entry-files/download/"
-                                          "{}_updated.cif".format(identifier))
-        r = fetch_from_url_or_retry(url, stream=True)
-        if r.ok:
-            with open(outputfile, 'wb') as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
 
-            if filename.endswith('.gz'):
-                with gzip.open(outputfile, 'rb') as infile, \
-                        open(outputfile.replace('.gz', ''), 'wb') as outfile:
-                    shutil.copyfileobj(infile, outfile)
-                    os.remove(outputfile)
+    if pdb:
+        url_endpoint = "entry-files/download/pdb{}.ent".format(identifier)
     else:
-        logger.debug("File %s exists ... skipping", outputfile.rstrip('.gz')) 
+        if bio:
+            # atom lines only?
+            # url_endpoint = ("static/entry/download/"
+            #                "{}-assembly-{}_atom_site.cif.gz".format(identifier, pref))
+            pref = get_preferred_assembly_id(identifier=identifier)
+            url_endpoint = ("static/entry/download/"
+                            "{}-assembly-{}.cif.gz".format(identifier, pref))
+        else:
+            # original mmCIF?
+            # url_endpoint = "entry-files/download/{}.cif".format(pdbid)
+            url_endpoint = "entry-files/download/{}_updated.cif".format(identifier)
+
+    url_root = config.http_pdbe
+    url = url_root + url_endpoint
+    Downloader(url=url, outputfile=outputfile,
+               decompress=True, override=override)
     return
 
 
@@ -218,28 +284,14 @@ def download_sifts_from_ebi(identifier, override=False):
     """
 
     filename = "{}.xml.gz".format(identifier)
-
     outputfile = os.path.join(config.db_root, config.db_sifts, filename)
     os.makedirs(os.path.join(config.db_root, config.db_sifts), exist_ok=True)
-    if not os.path.exists(outputfile.rstrip('.gz')) or override:
 
-        url = config.ftp_sifts + "{}.xml.gz".format(identifier)
-        try:
-            urllib.urlretrieve(url, outputfile)
-        except AttributeError:
-            # python 3.5+
-            urllib.request.urlretrieve(url, outputfile)
-        except IOError as e:
-            message = 'Unable to retrieve {} for {}'.format(url, str(e))
-            logger.critical(message)
-
-        if filename.endswith('.gz'):
-            with gzip.open(outputfile, 'rb') as infile, \
-                    open(outputfile.replace('.gz', ''), 'wb') as outfile:
-                shutil.copyfileobj(infile, outfile)
-                os.remove(outputfile)
-    else:
-        logger.debug("File %s exists ... skipping", outputfile.rstrip('.gz')) 
+    url_root = config.ftp_sifts
+    url_endpoint = "{}.xml.gz".format(identifier)
+    url = url_root + url_endpoint
+    Downloader(url=url, outputfile=outputfile,
+               decompress=True, override=override)
     return
 
 
@@ -256,19 +308,14 @@ def download_data_from_uniprot(identifier, file_format="fasta", override=False):
     file_format = file_format.lstrip('.')
     if file_format in ['txt', 'fasta', 'gff']:
         filename = "{}.{}".format(identifier, file_format)
-
         outputfile = os.path.join(config.db_root, config.db_uniprot, filename)
         os.makedirs(os.path.join(config.db_root, config.db_uniprot), exist_ok=True)
-        if not os.path.exists(outputfile) or override:
 
-            url = config.http_uniprot + "{}.{}".format(identifier, file_format)
-            r = fetch_from_url_or_retry(url, stream=True)
-            if r.ok:
-                with open(outputfile, 'wb') as f:
-                    r.raw.decode_content = True
-                    shutil.copyfileobj(r.raw, f)
-        else:
-            logger.debug("File %s exists ... skipping", outputfile.rstrip('.gz')) 
+        url_root = config.http_uniprot
+        url_endpoint = "{}.{}".format(identifier, file_format)
+        url = url_root + url_endpoint
+        Downloader(url=url, outputfile=outputfile,
+                   decompress=True, override=override)
     else:
         raise ValueError("File format {} is not currently implemented..."
                          "".format(file_format))
@@ -290,19 +337,14 @@ def download_alignment_from_cath(identifier, max_sequences=200, override=False):
         superfamily, funfam = identifier.split('_')[0], identifier.split('_')[1]
         outputfile = os.path.join(config.db_root, config.db_cath, filename)
         os.makedirs(os.path.join(config.db_root, config.db_cath), exist_ok=True)
-        if not os.path.exists(outputfile) or override:
 
-            url = config.http_cath + "superfamily/{}/funfam/{}/" \
-                                     "files/seed_alignment.fasta" \
-                                     "?max_sequences={}".format(superfamily, funfam,
-                                                                max_sequences)
-            r = fetch_from_url_or_retry(url, stream=True)
-            if r.ok:
-                with open(outputfile, 'wb') as f:
-                    r.raw.decode_content = True
-                    shutil.copyfileobj(r.raw, f)
-        else:
-            logger.debug("File %s exists ... skipping", outputfile.rstrip('.gz')) 
+        url_root = config.http_cath
+        url_endpoint = ("superfamily/{}/funfam/{}/files/seed_alignment.fasta"
+                        "?max_sequences={}".format(superfamily, funfam,
+                                                   max_sequences))
+        url = url_root + url_endpoint
+        Downloader(url=url, outputfile=outputfile,
+                   decompress=True, override=override)
     else:
         raise ValueError("Expected CATH  ID but got {}..."
                          "".format(identifier))
@@ -321,20 +363,15 @@ def download_alignment_from_pfam(identifier, alignment_size="seed",
     """
 
     filename = "{}.sth".format(identifier)
+    outputfile = os.path.join(config.db_root, config.db_pfam, filename)
+    os.makedirs(os.path.join(config.db_root, config.db_pfam), exist_ok=True)
 
-    outputfile = os.path.join(config.db_root, config.db_cath, filename)
-    os.makedirs(os.path.join(config.db_root, config.db_cath), exist_ok=True)
-    if not os.path.exists(outputfile) or override:
-
-        url = config.http_pfam + "family/{}/alignment/{}".format(identifier,
-                                                                 alignment_size)
-        r = fetch_from_url_or_retry(url, stream=True)
-        if r.ok:
-            with open(outputfile, 'wb') as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
-    else:
-        logger.debug("File %s exists ... skipping", outputfile.rstrip('.gz')) 
+    url_root = config.http_pfam
+    url_endpoint = ("family/{}/alignment/{}"
+                    "".format(identifier, alignment_size))
+    url = url_root + url_endpoint
+    Downloader(url=url, outputfile=outputfile,
+               decompress=True, override=override)
     return
 
 
